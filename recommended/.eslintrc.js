bde72e80a2800b83cf271002d86e3139280174c7
marcpre <marcpre@email.com>
2017-09-26 20:26:12 +0200

    update eslint file

--- recommended/.eslintrc.js
+++ recommended/.eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
         "indent": ["error", 4],
         "linebreak-style": ["error", "unix"],
         "quotes": ["error", "double"],
-        "semi": ["error", "always"],
+        "semi": ["error", "never"],
+        //work without semicolons
+        "no-unexpected-multiline": 2,
 
         // override default options for rules from base configurations
         "comma-dangle": ["error", "always"],
@@ -18,5 +20,8 @@ module.exports = {
         "ecmaVersion": 6,
         "sourceType": "module"
     },
+    "env": {
+        "node": true
+    }
 }
 
